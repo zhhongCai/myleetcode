1439e043bebd50c08509a7a9e6e1370ea3120d6f
caizh <zhhong.cai@gmail.com>
2020-12-30 22:02:41 +0800

    tree

---
+++ src/main/java/com/theonecai/leetcode/tree/TreeNode.java
@@ -0,0 +1,16 @@
+package com.theonecai.leetcode.tree;
+
+/**
+ * @Author: theonecai
+ * @Date: Create in 2020/12/30 21:23
+ * @Description:
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
