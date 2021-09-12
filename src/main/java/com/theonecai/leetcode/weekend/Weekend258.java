8ffc9b649e3833f6d4f1f3e837234a7797bf0914
zhhong.cai <zhhong.cai@gmail.com>
2021-09-12 09:56:04 +0800

    weekend258

---
+++ src/main/java/com/theonecai/leetcode/weekend/Weekend258.java
@@ -0,0 +1,32 @@
+package com.theonecai.leetcode.weekend;
+
+
+/**
+ * @Author: theonecai
+ * @Date: Create in 2021/09/12 10:24
+ * @Description:
+ */
+public class Weekend258 {
+
+
+
+    public static void main(String[] args) {
+        Weekend258 weekend = new Weekend258();
+        weekend.test();
+        weekend.test2();
+        weekend.test3();
+        weekend.test4();
+    }
+
+    private void test4() {
+    }
+
+    private void test3() {
+    }
+
+    private void test2() {
+    }
+
+    private void test() {
+    }
+}
